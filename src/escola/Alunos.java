de8f1b824e1a55fdfb120888bbade24cfa625a67
JorgeJr335 <jjorge335@gmail.com>
2019-10-03 11:59:06 -0300

    ta indo

--- src/escola/Alunos.java
+++ src/escola/Alunos.java
@@ -2,5 +2,76 @@
 package escola;
 
 class Alunos {
+    private int codAluno;
+    private String nome;
+    private int nota1;
+    private int nota2;
+    private int nota3;
+    private int nota4;
+    private int media;
+
+    public int getCodAluno() {
+        return codAluno;
+    }
+
+    public void setCodAluno(int codAluno) {
+        this.codAluno = codAluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public int getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(int nota2) {
+        this.nota2 = nota2;
+    }
+
+    public int getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(int nota3) {
+        this.nota3 = nota3;
+    }
+
+    public int getNota4() {
+        return nota4;
+    }
+
+    public void setNota4(int nota4) {
+        this.nota4 = nota4;
+    }
+
+    public int getMedia() {
+        return media;
+    }
+
+    public void setMedia(int media) {
+        this.media = (nota1+nota2+nota3+nota4)/4;
+        
+    }
     
+    public String exibirSituacao(){
+        if(media >=7){
+        return "Passou";
+        } else {
+        return "Não passou";
+        }
+    }
 }
--- src/escola/Main.java
+++ src/escola/Main.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package escola;
 
-/**
- *
- * @author Aluno01
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Escola LuizAbel = new Escola();
+        LuizAbel.setNome("Luiz Abel");
+        LuizAbel.turma
+        
+        Turma NonoA = new Turma();
+        NonoA.setNomeTurma("9A");
+        NonoA.alunos;
+        
+        Turma NonoD = new Turma();
+        NonoD.setNomeTurma("9D");
+        NonoD.alunos;
     }
     
 }
